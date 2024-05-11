bd386c51ba99b5e68d675d5763de8b9fff06aa70
Gabriel Vieira <gabriel_silvarosa12@hotmail.com>
2024-05-11 15:24:15 -0300

    Atualizando HTML e JS

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,9 +1,11 @@
 window.onload = function() {
     document.querySelector(".menu-opener").addEventListener("click", function(){
+
     if(document.querySelector(".menu nav").style.display == 'flex') {
        document.querySelector(".menu nav").style.display = 'none';
     } else {
        document.querySelector(".menu nav").style.display = 'flex';
     }
+    
 });
 };
